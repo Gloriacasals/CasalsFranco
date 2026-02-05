047d0c235c47e842384130a581651ccef3dca7ad
AngelD222 <angelfranco2000@gmail.com>
2026-02-05 17:05:19 +0100

    Create __init__.py

---
+++ k_brazos/src/plotting/__init__.py
@@ -0,0 +1,10 @@
+"""
+Module: plotting/__init__.py
+Description: Contiene las importaciones y modulos/clases públicas del paquete plotting.
+"""
+
+# Importación de módulos o clases
+from .plotting import plot_average_rewards, plot_optimal_selections, plot_regret, plot_arm_statistics
+
+# Lista de módulos o clases públicas
+__all__ = ['plot_average_rewards', 'plot_optimal_selections', 'plot_regret', 'plot_arm_statistics']
